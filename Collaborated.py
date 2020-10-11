4147af1943f8f4cb46e9bddeace1093d6b90543c
Prasannakumar-kumta <50964430+Prasannakumar-kumta@users.noreply.github.com>
2020-10-12 02:03:25 +0530

    Re comitt

--- Collaborated.py
+++ Collaborated.py
@@ -2057,3 +2057,178 @@ pizza.pineapple_allowed = True
 print(pizza.pineapple_allowed)
 
 #Tap Try It Yourself to play around with the code!
+
+''''Regular Expressions
+
+Regular expressions are a powerful tool for various kinds of string manipulation.
+They are a domain specific language (DSL) that is present as a library in most
+modern programming languages, not just Python.
+They are useful for two main tasks:
+- verifying that strings match a pattern (for instance, that a string has the
+format of an email address),
+- performing substitutions in a string (such as changing all American spellings
+to British ones).
+Domain specific languages are highly specialized mini programming languages.
+Regular expressions are a popular example, and SQL (for database manipulation)
+is another.
+Private domain-specific languages are often used for specific industrial purposes.''''
+
+''''Regular expressions in Python can be accessed using the re module, which is part
+of the standard library.
+After you've defined a regular expression, the re.match function can be used to
+determine whether it matches at the beginning of a string.
+If it does, match returns an object representing the match, if not, it returns
+None.
+To avoid any confusion while working with regular expressions, we would use raw
+strings as r"expression".
+Raw strings don't escape anything, which makes use of regular expressions easier.
+''''
+#Example:
+import re
+
+pattern = r"spam"
+
+if re.match(pattern, "spamspamspam"):
+   print("Match")
+else:
+   print("No match")
+
+''''The above example checks if the pattern "spam" matches the string and prints
+"Match" if it does.
+
+Here the pattern is a simple word, but there are various characters, which would
+have special meaning when they are used in a regular expression.''''
+
+''''Other functions to match patterns are re.search and re.findall.
+The function re.search finds a match of a pattern anywhere in the string.
+The function re.findall returns a list of all substrings that match a pattern.''''
+#Example:
+import re
+pattern = r"spam"
+
+if re.match(pattern, "eggspamsausagespam"):
+   print("Match")
+else:
+   print("No match")
+
+if re.search(pattern, "eggspamsausagespam"):
+   print("Match")
+else:
+   print("No match")
+
+print(re.findall(pattern, "eggspamsausagespam"))
+
+''''In the example above, the match function did not match the pattern, as it looks
+at the beginning of the string.
+The search function found a match in the string.
+
+The function re.finditer does the same thing as re.findall, except it returns
+an iterator, rather than a list.''''
+''''The regex search returns an object with several methods that give details about
+it.
+These methods include group which returns the string matched, start and end which
+ return the start and ending positions of the first match, and span which returns
+ the start and end positions of the first match as a tuple.''''
+#Example:
+import re
+
+pattern = r"pam"
+
+match = re.search(pattern, "eggspamsausage")
+if match:
+   print(match.group())
+   print(match.start())
+   print(match.end())
+   print(match.span())
+
+''''Search & Replace
+One of the most important re methods that use regular expressions is sub.''''
+#Syntax:
+re.sub(pattern, repl, string, count=0)
+
+''''This method replaces all occurrences of the pattern in string with repl,
+substituting all occurrences, unless count provided. This method returns
+the modified string.''''
+#Example:
+import re
+
+str = "My name is David. Hi David."
+pattern = r"David"
+newstr = re.sub(pattern, "Amy", str)
+print(newstr)
+
+''''Groups
+A group can be created by surrounding part of a regular expression with parentheses.
+This means that a group can be given as an argument to metacharacters such as * and ?.'''
+#Example:
+import re
+
+pattern = r"egg(spam)*"
+
+if re.match(pattern, "egg"):
+   print("Match 1")
+
+if re.match(pattern, "eggspamspamspamegg"):
+   print("Match 2")
+
+if re.match(pattern, "spam"):
+   print("Match 3")
+
+''''The content of groups in a match can be accessed using the group function.
+A call of group(0) or group() returns the whole match.
+A call of group(n), where n is greater than 0, returns the nth group from the left.
+The method groups() returns all groups up from 1.'''
+#Example:
+import re
+
+pattern = r"a(bc)(de)(f(g)h)i"
+
+match = re.match(pattern, "abcdefghijklmnop")
+if match:
+   print(match.group())
+   print(match.group(0))
+   print(match.group(1))
+   print(match.group(2))
+   print(match.groups())
+
+ #As you can see from the example above, groups can be nested.
+
+ ''''There are several kinds of special groups.
+ Two useful ones are named groups and non-capturing groups.
+ Named groups have the format (?P<name>...), where name is the name of the group,
+  and ... is the content. They behave exactly the same as normal groups, except
+  they can be accessed by group(name) in addition to its number.
+ Non-capturing groups have the format (?:...). They are not accessible by the
+ group method, so they can be added to an existing regular expression without
+ breaking the numbering'''.
+ #Example:
+
+ import re
+
+ pattern = r"(?P<first>abc)(?:def)(ghi)"
+
+ match = re.match(pattern, "abcdefghi")
+ if match:
+    print(match.group("first"))
+    print(match.groups())
+
+''''Metacharacters.
+Another important metacharacter is |.
+This means "or", so red|blue matches either "red" or "blue".''''
+#Example:
+import re
+
+pattern = r"gr(a|e)y"
+
+match = re.match(pattern, "gray")
+if match:
+   print ("Match 1")
+
+match = re.match(pattern, "grey")
+if match:
+   print ("Match 2")
+
+match = re.match(pattern, "griy")
+if match:
+    print ("Match 3")
+    
