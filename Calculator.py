24f5b79e866bebf7fd63b28f86e7988ec1249ab4
Prasannakumar-kumta <50964430+Prasannakumar-kumta@users.noreply.github.com>
2020-10-14 16:05:20 +0530

    Comitted

---
+++ Calculator.py
@@ -0,0 +1,8 @@
+print("What you want to calculate? \n1)Basic calculation \n2)Complex calculation")
+print("Enter 1 or 2:")
+x=input (x)
+if x==1:
+    Basic()
+
+def basic ():
+    print("Which Basic operation do you want to perform?")
--- Helloworld.py
+++ Helloworld.py
@@ -1,15 +1,12 @@
 import re
+pattern= r"(.)\1"
 
-pattern = r"gr(a|e)y"
-
-match = re.match(pattern, "gray")
+match=re.match(pattern, "abc spam")
 if match:
-   print ("Match 1")
-
-match = re.match(pattern, "grey")
+    print("Match 1")
+match=re.match(pattern, "?! ?!")
 if match:
-   print ("Match 2")
-
-match = re.match(pattern, "griy")
+    print("Match 2")
+match=re.match(pattern, "abc cde")
 if match:
-    print ("Match 3")
+    print("Match 3")
